38c93ecd06efad59284d2abe40b2caa9154568b1
Shirsendu <palshirsendu03@gmail.com>
2024-07-01 02:49:44 +0530

    Minor issues

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ movies_list=df['title'].values
 similarity=pickle.load(open('similarity.pkl','rb'))
 
 def fetch_poster(movie_id):
-    response=requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=English')
+    response=requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=e486a81162504754c03ee6845fe54000&language=English')
     data=response.json()
     print(data)
     return "http://image.tmdb.org/t/p/w500/"+data['poster_path']
